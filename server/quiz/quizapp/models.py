104989f625841902ae80be67a28566bef310816b
Marco Serena <marcoserena96@gmail.com>
2024-05-14 17:29:49 +0200

    First structure of django

---
+++ server/quiz/quizapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Game(models.Model):
+    pass
+
+
+class Question(models.Model):
+    pass
+
+
+class UserAnswer(models.Model):
+    pass
