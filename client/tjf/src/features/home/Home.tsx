4573bb34434f1d9e5c3eca64e6d57d629dfc3770
GiovaS80 <giomain.pv@gmail.com>
2024-05-15 12:19:52 +0700

    update HomePage for get IP Address of user

--- client/tjf/src/features/home/Home.tsx
+++ client/tjf/src/features/home/Home.tsx
@@ -5,92 +5,147 @@ import ButtonComponent from "../../shared/design/button/ButtonComponent";
 import "../../shared/design/button/button.scss";
 import "../../index.css";
 //import React, { useRef } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faVolumeXmark, faVolumeHigh } from '@fortawesome/free-solid-svg-icons';
-import React, { useRef, useEffect } from 'react';
-
-
-
-
-
-
-
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeXmark, faVolumeHigh } from "@fortawesome/free-solid-svg-icons";
+import React, { useRef, useEffect, useState } from "react";
 
 const Home: React.FC = () => {
-
-
-  const audioRef = useRef<HTMLAudioElement>(null); 
+  const audioRef = useRef<HTMLAudioElement>(null);
 
   const [isAudioMuted, setIsAudioMuted] = React.useState(false);
-  
+
   const toggleAudio = () => {
-    
-    setIsAudioMuted(prevState => !prevState); 
+    setIsAudioMuted((prevState) => !prevState);
     if (audioRef.current) {
       if (audioRef.current.paused) {
-        audioRef.current.play(); 
+        audioRef.current.play();
       } else {
-        audioRef.current.pause(); 
+        audioRef.current.pause();
       }
     }
   };
 
+  const [ipAddress, setIpAddress] = useState("");
 
- 
+  const fetcIP = async () => {
+    try {
+      const response = await fetch("https://api.ipify.org");
+      const data = await response.text();
+      setIpAddress(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const newGame = () => {
     console.log("Sono Azione Uno");
   };
 
   const codeGame = () => {
-    console.log("Sono Azione Due"); 
+    console.log("Sono Azione Due");
   };
+
+  useEffect(() => {
+    fetcIP();
+  }, []);
 
   return (
     <>
-    
+      <audio ref={audioRef} src="/backgroundmusic.mp3" loop preload="auto" />
 
-      
-      <audio ref={audioRef} src="/backgroundmusic.mp3" loop preload="auto"  />
-
-      
       <div className="audio-toggle" onClick={toggleAudio}>
-      {isAudioMuted ? <FontAwesomeIcon icon={faVolumeHigh}  /> : <FontAwesomeIcon icon={faVolumeXmark}/>}   
-      
+        {isAudioMuted ? (
+          <FontAwesomeIcon icon={faVolumeHigh} />
+        ) : (
+          <FontAwesomeIcon icon={faVolumeXmark} />
+        )}
       </div>
 
-
-    
       <div>
-        
-          <img className="logo" src="./logoquiz.jpeg" alt="" />
-       
+        <img className="logo" src="./logoquiz.jpeg" alt="" />
       </div>
       <div>
-        <h1 style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>Benvenuto!</h1>
+        <h1
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          Benvenuto!
+        </h1>
 
-        <h2 className="h2home" style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-
-        inizia una nuova partita o inserisci un codice per entrare in una stanza
+        <h2
+          className="h2home"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          inizia una nuova partita o inserisci un codice per entrare in una
+          stanza
         </h2>
       </div>
       {/* <div>
         <h3>inserisci il tuo nome</h3>
       </div> */}
-      <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-     
-        <input type="text" id="name" className="input-text" placeholder="Inserisci Nome"/>
-        </div>
-      <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-        {/* Passa le classi CSS come prop al componente ButtonComponent */}
-        <ButtonComponent text="Inizia Nuova Partita" clickButton={newGame} classNames="custom-btn custom-btn-primary button-19" />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <input
+          type="text"
+          id="name"
+          className="input-text"
+          placeholder="Inserisci Nome"
+        />
       </div>
-      <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <input type="text" id="code" className="input-text" placeholder="Codice partita"/>
-      </div>
-      <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         {/* Passa le classi CSS come prop al componente ButtonComponent */}
-        <ButtonComponent text="Usa Codice" clickButton={codeGame} classNames="btn btn-primary" />
+        <ButtonComponent
+          text="Inizia Nuova Partita"
+          clickButton={newGame}
+          classNames="custom-btn custom-btn-primary button-19"
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <input
+          type="text"
+          id="code"
+          className="input-text"
+          placeholder="Codice partita"
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {/* Passa le classi CSS come prop al componente ButtonComponent */}
+        <ButtonComponent
+          text="Usa Codice"
+          clickButton={codeGame}
+          classNames="btn btn-primary"
+        />
       </div>
     </>
   );
