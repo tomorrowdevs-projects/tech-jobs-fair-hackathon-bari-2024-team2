23cbabac2032ab4c75a5dca5af67d916b2c1460c
GiovaS80 <giomain.pv@gmail.com>
2024-05-15 12:17:35 +0700

    update NewGame Page and route for NewGame Page

--- client/tjf/src/App.tsx
+++ client/tjf/src/App.tsx
@@ -4,6 +4,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import React from "react";
 import Home from "./features/home/Home";
 import Game from "./features/game/game";
+import NewGame from "./features/newgame/NewGame";
 
 const App = () => {
   // useContext(){}
@@ -14,6 +15,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/game" element={<Game />}></Route>
+            <Route path="/newgame" element={<NewGame />}></Route>
           </Routes>
         </div>
       </Router>
--- client/tjf/src/features/newgame/NewGame.tsx
+++ client/tjf/src/features/newgame/NewGame.tsx
@@ -1,14 +1,86 @@
 import ButtonComponent from "../../shared/design/button/ButtonComponent";
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
 
 const NewGame: React.FC = () => {
   const azioneUno = () => {
     console.log("Sono Azione NewGame");
   };
+
+  
+  const name = "nome";
+
+  const handleSubmit1 = (
+    data:any) => {
+    setData(JSON.stringify(data));
+    console.log('Form submitted:');
+    console.log(data);
+  }
+  const { register , handleSubmit} = useForm();
+  const [data, setData] = useState("");
+
   return (
     <>
-      <h1>Sono la page NewGame ed ho un bottone</h1>
-      <ButtonComponent text="Azione NewGame" clickButton={azioneUno} />
+      <h1>Benvenuto {name}</h1>
+      <h3>
+        Ti ricordiamo che puoi fare delle scelte o lasciare le impostazioni come
+        le vedi
+      </h3>
+      <form 
+      onSubmit={handleSubmit((data:any) => {handleSubmit1(data)})} 
+      action="" method="post">
+        <label htmlFor="category">Seleziona la categoria</label>
+        <select 
+        {...register("category")} 
+        name="category">
+          <option value="any">Any Category</option>
+          <option value="9">General Knowledge</option>
+          <option value="10">Entertainment: Books</option>
+          <option value="11">Entertainment: Film</option>
+          <option value="12">Entertainment: Music</option>
+          <option value="13">Entertainment: Musicals &amp; Theatres</option>
+          <option value="14">Entertainment: Television</option>
+          <option value="15">Entertainment: Video Games</option>
+          <option value="16">Entertainment: Board Games</option>
+          <option value="17">Science &amp; Nature</option>
+          <option value="18">Science: Computers</option>
+          <option value="19">Science: Mathematics</option>
+          <option value="20">Mythology</option>
+          <option value="21">Sports</option>
+          <option value="22">Geography</option>
+          <option value="23">History</option>
+          <option value="24">Politics</option>
+          <option value="25">Art</option>
+          <option value="26">Celebrities</option>
+          <option value="27">Animals</option>
+          <option value="28">Vehicles</option>
+          <option value="29">Entertainment: Comics</option>
+          <option value="30">Science: Gadgets</option>
+          <option value="31">Entertainment: Japanese Anime &amp; Manga</option>
+          <option value="32">
+            Entertainment: Cartoon &amp; Animations
+          </option>{" "}
+        </select>
+        
+
+
+        <label htmlFor="difficulty">Select Difficulty: </label>
+        <select {...register("difficulty")} name="difficulty">
+          <option value="any">Any Difficulty</option>
+          <option value="easy">Easy</option>
+          <option value="medium">Medium</option>
+          <option value="hard">Hard</option>
+        </select>
+
+        <label htmlFor="type">Select Type: </label>
+        <select {...register("type")}  name="type">
+          <option value="any">Any Type</option>
+          <option value="multiple">Multiple Choice</option>
+          <option value="boolean">True / False</option>
+        </select>
+        <ButtonComponent text="Avvia Quiz" type="submit" />
+      </form>
     </>
   );
 };
