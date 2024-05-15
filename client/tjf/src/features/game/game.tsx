7b875ac879dfb91a688585a4aff0c3455e86dbca
GiovaS80 <giomain.pv@gmail.com>
2024-05-15 23:06:45 +0700

    start time

--- client/tjf/src/features/game/game.tsx
+++ client/tjf/src/features/game/game.tsx
@@ -1,13 +1,24 @@
 import ButtonComponent from "../../shared/design/button/ButtonComponent";
-import React from "react";
+import React, { useState } from "react";
 import Question from "./question";
+import CountDown from "./countDown";
 
 const Game: React.FC = () => {
+  const [userAnswer, setUserAnswer] = useState<string | null>(null);
+  const [hasTimeRunOut, setHasTimeRunOut] = useState<boolean>(false);
+
+  const setTime = ()=> {
+    setHasTimeRunOut(!hasTimeRunOut);
+    console.log("hasTimeRunOut");
+    console.log(hasTimeRunOut);
+    
+  }
   const azioneUno = () => {
     console.log("Sono Azione Game");
   };
   return (
     <>
+      <CountDown timeRunOut={setTime} ></CountDown>
       <Question />
       <h1>Sono la page Game ed ho un bottone</h1>
       {/* <ButtonComponent text="Azione Game" clickButton={azioneUno} /> */}
